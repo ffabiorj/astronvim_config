feb81e76b4939439771a07d79f2c2bd9c4affaab
Fabio  Oliveira <fabio20rj@gmail.com>
2024-01-22 17:25:32 +0100

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -8,6 +8,15 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    -- Moving like vscode
+    ["<A-j>"] = {":m .+1<CR>=="},
+    ["<A-k>"] = {":m .-2<CR>=="},
+    -- kept the cursour in the middle
+    ["<C-d>"] =  {"<C-d>zz"},
+    ["<C-u>"] = {"<C-u>zz"},
+    -- movie buffers 
+    ["<S-h>"] = { ":bprevious<cr>"},
+    ["<S-l>"] = { ":bnext<cr>"},
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
@@ -24,5 +33,16 @@ return {
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  v = {
+        -- Better indenting
+    ["<"] = {"<gv"},
+    [">"] = {">gv"},
+  },
+  i = {
+   -- Move current line / block with Alt-j/k ala vscode.
+    ["<A-j>"] = {"<Esc>:m .+1<CR>==gi"},
+    -- Move current line / block with Alt-j/k ala vscode.
+    ["<A-k>"] = {"<Esc>:m .-2<CR>==gi"},
   },
 }
