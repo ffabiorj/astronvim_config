2975af26f01d0da230afeca92364a05119df109b
Fabio  Oliveira <fabio20rj@gmail.com>
2024-01-30 17:22:53 +0100

    Update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,16 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+    { "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+      },
+      transparent_background = true,
+    }
+  }
+
 }
